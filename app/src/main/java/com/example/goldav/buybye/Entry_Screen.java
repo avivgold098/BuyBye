a28089d4da9d610a3102c1f14152faaccf848d0d
Aviv Gold <avivgold098@gmail.com>
2017-06-13 01:46:57 +0300

    check

--- app/src/main/java/com/example/goldav/buybye/Entry_Screen.java
+++ app/src/main/java/com/example/goldav/buybye/Entry_Screen.java
@@ -18,7 +18,7 @@ import android.widget.Button;
  * to handle interaction events.
  * Use the {@link Entry_Screen#newInstance} factory method to
  * create an instance of this fragment.
- */
+ *///checkech
 public class Entry_Screen extends Fragment  {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
